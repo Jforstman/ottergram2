260fd2cedb553cd891229fcafece9a40a45ab7ac
JForstman <justlovinglife73@gmail.com>
2023-06-22 11:21:27 -0500

    inital commit

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+function Footer () {
+  return (
+  <footer className='footer-component'>
+        <h3>Built by: Jamie Forstman</h3>
+      </footer>
+  )
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header () {
+  return (
+  <header className='header-component'>
+        <h1>Ottergram</h1>
+        <h3>the page that is all about otters!</h3>
+      </header>
+  )
+}
+export default Header;
